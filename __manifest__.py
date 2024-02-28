b03f2381a1716064e3697a0c4cc50619b0d83a24
RuiRosaa <ruipedrofrosa@gmail.com>
2024-02-28 16:10:57 +0000

    [ADD] Project base

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Delivery Team Management',
+    'author': 'Rui Pedro',
+    'summary': 'Odoo 17 Delivery Team',
+    'depends': ['sale','base'],
+    'data': [
+        #'security/ir.model.access.csv',
+        #'views/patient.xml',
+        #'views/menu.xml'
+    ]
+}
