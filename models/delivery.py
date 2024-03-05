5c558dc0be11ce1757c0516304b6fff15c178802
RuiRosaa <ruipedrofrosa@gmail.com>
2024-03-05 17:53:58 +0000

    [IMP] view delivery team

--- models/delivery.py
+++ models/delivery.py
@@ -2,14 +2,23 @@ from odoo import api, fields, models, _
 
 
 class DeliveryTeam(models.Model):
+    _inherit = 'stock.picking'
     _name = 'delivery.team'
     _description = 'Delivery Team'
 
-    name = fields.Char(string='Team Name')
+    name = fields.Char(string='Batch Transfer')
 
 
+class StockPickingBatch(models.Model):
+    _inherit = 'stock.picking.batch'
 
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    delivery_team_id = fields.Many2one('delivery.team', string='Delivery Team', help='Select the delivery team')
 
-   # delivery_team = fields.Many2one('delivery.team', string='Delivery Team', ondelete={'cascade': 'set null'})
+
+#class StockPicking(models.Model):
+     #_inherit = 'stock.picking.batch'
+
+    #name = fields.Char(string='Delivery Team')
+    #delivery_team = fields.Many2one('delivery.team', string='Delivery Team', ondelete={'cascade': 'set null'})
+
+
